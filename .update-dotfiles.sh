261723958fe8e54fcffdedf9fc892d263df68121
David Strauß <david@strauss.io>
2020-05-25 17:39:54 +0200

    Add Espanso for text expanding

--- .update-dotfiles.sh
+++ .update-dotfiles.sh
@@ -8,10 +8,12 @@ if [ "$(uname)" == "Darwin" ]; then
   # macOS
 
   SUBLIME_PACKAGES_DIRECTORY=~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+  ESPANSO_CONFIG_DIRECTORY=~/Library/Preferences/espanso
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # Linux
 
   SUBLIME_PACKAGES_DIRECTORY=~/.config/sublime-text-3/Packages/
+  ESPANSO_CONFIG_DIRECTORY="$XDG_CONFIG_HOME/espanso"
 fi
 
 # Setup Sublime Text 3 by symlinking User settings directory
@@ -23,6 +25,17 @@ fi
 
 if ! [ -L User ]; then
   ln -s ~/.sublime-text-3/User
+fi
+
+# Setup Espanso by symlinking configuration
+cd "$ESPANSO_CONFIG_DIRECTORY"
+
+if [[ -f default.yml && ! -L default.yml ]]; then
+  mv default.yml default.yml.backup
+fi
+
+if ! [ -L default.yml ]; then
+  ln -s ~/.espanso-config/default.yml
 fi
 
 # Copy git hooks to enable self updating
